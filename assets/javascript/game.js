e22453a41cfc29859379dca7baace7f76bab2d16
John Pendergrass <john.pendergrass@me.com>
2020-02-19 16:58:46 -0600

    removed jumbotron, added background picture, wrote some pseudocode for js

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+
+
+
+// 90s movie array
+var movies = ["Heat", "Se7en", "Goodfellas", "Misery", "Misery", "Aladdin"];
+   
+// Use math random to choose random word in movie array
+
+// Use forloop to use words in the array - whatever length you have, use a dash for each letter
+
+// everytime you guess a letter, and it matches letters in the word, push the letter where the dash is, if not, reduce the number of tries
