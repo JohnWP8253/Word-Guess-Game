6f1b3aea3ef559966fb4f6cb8618c7f965f59339
John Pendergrass <john.pendergrass@me.com>
2020-02-25 10:55:19 -0600

    added cinema image, movie posters and wins section

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,12 @@
 
 
 
-// 90s movie array
-var movies = ["Heat", "Se7en", "Goodfellas", "Misery", "Misery", "Aladdin"];
+// 90s movie guessing game
+// var movies = ["heat", "se7en", "goodfellas", "misery", "philadelphia", "aladdin", "clerks"];
+
+var movieGuessingGame = {
+    moviesToGuess: {
+
+    }
+}
    
-// Use math random to choose random word in movie array
-
-// Use forloop to use words in the array - whatever length you have, use a dash for each letter
-
-// everytime you guess a letter, and it matches letters in the word, push the letter where the dash is, if not, reduce the number of tries
